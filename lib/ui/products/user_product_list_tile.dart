68a750af131b54de735092ac320694b358d46bfb
VyDinhQuang-B2014689 <kohakida@gmail.com>
2022-10-30 14:18:43 +0700

    Xay dung trang hieu chinh san pham

--- lib/ui/products/user_product_list_tile.dart
+++ lib/ui/products/user_product_list_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:myshop/models/product.dart';
+import 'package:myshop/ui/products/edit_product_screen.dart';
 import 'package:myshop/ui/products/products_manager.dart';
 import 'package:provider/provider.dart';
 
@@ -55,7 +56,10 @@ class UserProductListTile extends StatelessWidget {
     return IconButton(
       icon: const Icon(Icons.edit),
       onPressed: () {
-        print('Go to edit product screen');
+        Navigator.of(context).pushNamed(
+          EditProductScreen.routeName,
+          arguments: product.id,
+        );
       },
       color: Theme.of(context).primaryColor,
     );
