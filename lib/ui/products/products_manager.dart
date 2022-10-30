e737c2e5b4bc3df71ae5a8ec65fb1cc1569125a4
VyDinhQuang-B2014689 <kohakida@gmail.com>
2022-10-30 16:21:28 +0700

    Cap nhat, xoa san pham tren Realtime Database cua Firebase

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -45,11 +45,13 @@ class ProductsManager with ChangeNotifier {
     }
   }
 
-  void updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     final index = _items.indexWhere((item) => item.id == product.id);
     if (index >= 0) {
-      _items[index] = product;
-      notifyListeners();
+      if (await _productsService.updateProduct(product)) {
+        _items[index] = product;
+        notifyListeners();
+      }
     }
   }
 
@@ -58,9 +60,15 @@ class ProductsManager with ChangeNotifier {
     product.isFavorite = !savedStatus;
   }
 
-  void deleteProduct(String id) {
+  Future<void> deleteProduct(String id) async {
     final index = _items.indexWhere((item) => item.id == id);
+    Product? existingProduct = _items[index];
     _items.removeAt(index);
     notifyListeners();
+
+    if (!await _productsService.deleteProduct(id)) {
+      _items.insert(index, existingProduct);
+      notifyListeners();
+    }
   }
 }
